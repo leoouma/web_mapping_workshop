5117c6537e4c2a68fe6b204715298518f94a52b0
Leonard Ouma <leoouma@users.noreply.github.com>
2016-06-04 15:15:46 +0300

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -41,3 +41,37 @@ featureLayer.on('ready', function() {
 //    layer.bindPopup('Account opening available? ' + layer.feature.properties.Account_Op);
 //  });
 //});
+
+
+//Start by adding a click handler function that takes the click event as a variable called 'e':
+
+var clickHandler = function(e){
+  $('#info').empty();
+
+  var feature = e.target.feature;
+
+  $('#sidebar').fadeIn(400,function(){
+    var info = '';
+
+    info += '<div>'
+    info += '<h2>' + feature.properties.County + '</h2>'
+    if(feature.properties.Other_Busi) info +=   '<p>'  + feature.properties.Trading_Ho + '</p>'
+    if(feature.properties.Account_Op) info +=   '<p>'  + feature.properties.Account_Op + '</p>'
+    if(feature.properties.Account_Op) info +=   '<p>'  + feature.properties.GPS_Accura + '</p>'
+    if(feature.properties.Account_Op) info +=   '<p><a href="' + feature.properties.GPS_Accura + '">'  + feature.properties.GPS_Accura + '</a></p>'
+    info += '</div>'
+
+    $('#info').append(info);
+  });
+};
+
+featureLayer.on('ready', function(){
+  this.eachLayer(function(layer){
+    layer.on('click', clickHandler);
+  });
+});
+
+map.on('click',function(e){
+    $('#info').fadeOut(200);
+    $('#info').empty();
+});
