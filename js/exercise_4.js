e5d5359bb217f20761e280b6f0b07aec9c9cc7fa
Leonard Ouma <leoouma@users.noreply.github.com>
2016-06-04 14:55:48 +0300

    Added Popup

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -38,6 +38,6 @@ featureLayer.on('ready', function() {
 // Adding popup
 featureLayer.on('ready', function(){
   this.eachLayer(function(layer){
-    layer.bindPopup('Welcome to ' + layer.feature.properties.name);
+    layer.bindPopup('Welcome to ' + layer.feature.properties.Other_Busi);
   });
 });
